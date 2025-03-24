765aabeeb8febe0fa4297c303a97c016bbb17de5
Victor Wu <vicwu.off@gmail.com>
2025-03-24 15:12:16 -0400

    Initial Commit

---
+++ MeowMeows/MeowMeowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeowMeowsApp.swift
+//  MeowMeows
+//
+//  Created by Victor Wu on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MeowMeowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeowMeowsTests/MeowMeowsTests.swift
@@ -0,0 +1,16 @@
+//
+//  MeowMeowsTests.swift
+//  MeowMeowsTests
+//
+//  Created by Victor Wu on 3/24/25.
+//
+
+import Testing
+
+struct MeowMeowsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
